73a399d6a0650665253685fcc8a0905e4be9a85a
yzehao <yzehao@thoughtworks.com>
2019-11-14 23:18:11 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bulldog
+
+##Introduction
+This is a road analysis software
+
+##Technology stack
+* java8
+* springboot
+* gradle
+* javafx 
---
+++ src/main/java/com/nrty/bulldog/BulldogApplication.java
@@ -0,0 +1,46 @@
+package com.nrty.bulldog;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BulldogApplication extends Application {
+    private ConfigurableApplicationContext springContext;
+    private FXMLLoader fxmlLoader;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(BulldogApplication.class);
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(springContext::getBean);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        fxmlLoader.setLocation(getClass().getResource("/fxml/sample.fxml"));
+        Parent rootNode = fxmlLoader.load();
+
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(rootNode, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.stop();
+    }
+}
